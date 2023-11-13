9f2f26760dae1fb951d13b7ddea56d9c2170d5d2
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2023-11-13 13:49:28 +0200

    fixes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,11 @@
-import { UserMenu } from 'components/UserMenu/UserMenu';
-import React, { Suspense } from 'react';
+import React from 'react';
+
 import { useSelector } from 'react-redux';
 import { NavLink, Outlet } from 'react-router-dom';
 import { selectIsLoggedIn } from 'redux/auth/authSelectors';
+
+import { UserMenu } from 'components/UserMenu/UserMenu';
+
 import s from './Layout.module.css';
 
 export const Layout = () => {
